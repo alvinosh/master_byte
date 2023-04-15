4b65ee1d1f53481778dd7d444a17a313f39e981c
Alvinosh <alvihysa89@gmail.com>
2023-04-15 19:23:02 +0200

    Wtf

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-gcc -Wall -Wextra -pedantic -std=c99 ./src/**.c -I./src/ -o ./build/main.a  && ./build/main.a
+gcc -Wall -Wextra -pedantic -std=c99 ./src/**.c -I./src/ -o ./build/main  && ./build/main
